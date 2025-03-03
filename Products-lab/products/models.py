d630e7c8a8622b47b683464c88b7ffd7e150cc94
Ahmed Mazen <ahmedmazenm@outlook.com>
2025-03-04 01:59:52 +0200

    cart model added

--- Products-lab/products/models.py
+++ Products-lab/products/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
+from django.contrib.auth.models import User
 
 class Category(models.Model):
     name = models.CharField(max_length=100, unique=True)
@@ -24,3 +25,9 @@ class Product(models.Model):
 
     def __str__(self):
         return self.title
+    
+class Cart(models.Model):
+    product = models.ForeignKey(Product,on_delete=models.CASCADE)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    date_created = models.DateTimeField(auto_now_add=True)
+
