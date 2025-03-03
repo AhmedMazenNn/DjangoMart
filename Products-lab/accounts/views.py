adc9c880641c8db0786aa6e2f9927965481643fa
Ahmed Mazen <ahmedmazenm@outlook.com>
2025-03-04 01:47:28 +0200

    added accounts app to handel user login sign up logout and profile the profile is still in work

---
+++ Products-lab/accounts/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render ,redirect
+from django.contrib.auth import logout
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .forms import UserRegisterForm
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request , f'Account created for {username}')
+            return redirect('login')
+    else:
+        form = UserRegisterForm()
+    return render(request , 'accounts/register.html' , {'form' : form})
+
+
+def logout_view(request):
+    if request.method == "POST":
+        logout(request)
+        return redirect("login")  
+    return render(request, "accounts/logout.html")
+
+@login_required
+def profile(request):
+    return render(request, 'accounts/profile.html')
