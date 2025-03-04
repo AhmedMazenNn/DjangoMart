34c4326af2c2a9afb34f7ea2492d9ee30cb71c94
Ahmed Mazen <ahmedmazenm@outlook.com>
2025-03-04 02:01:57 +0200

    modified project settings and urls

--- Products-lab/Products/settings.py
+++ Products-lab/Products/settings.py
@@ -38,7 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'products'
+    'products',
+    'accounts',
+    'crispy_forms',
+    'crispy_bootstrap5',
 ]
 
 MIDDLEWARE = [
@@ -129,5 +132,12 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
+
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap5"
+
+LOGIN_REDIRECT_URL = 'all_products'
+
+LOGIN_URL = 'login'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
--- Products-lab/Products/urls.py
+++ Products-lab/Products/urls.py
@@ -15,13 +15,22 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path , include
 from django.conf import settings
 from django.conf.urls.static import static
+from accounts import views as account_views
 
 urlpatterns = ([
     path('admin/', admin.site.urls),
-    path('', include('products.urls'))
+    path('', include('products.urls')),
+    path('register/' , account_views.register, name='register'),
+    path('login/' , auth_views.LoginView.as_view(template_name ='accounts/login.html'), name='login'),
+    path('logout/' , account_views.logout_view, name='logout'),
+    path('profile/' , account_views.profile, name='profile'),
+
+
+
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
             + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT))
 
