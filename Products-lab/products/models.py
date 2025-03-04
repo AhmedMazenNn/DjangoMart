9208060aef48bff0251fe6fe23ec28d3d4a8532e
Ahmed Mazen <ahmedmazenm@outlook.com>
2025-03-04 14:12:38 +0200

    added validation when admin add product and category

--- Products-lab/products/models.py
+++ Products-lab/products/models.py
@@ -1,9 +1,15 @@
 from django.db import models
-from django.core.validators import MaxValueValidator, MinValueValidator
+from django.core.validators import MaxValueValidator, MinValueValidator ,RegexValidator
 from django.contrib.auth.models import User
 
 class Category(models.Model):
-    name = models.CharField(max_length=100, unique=True)
+    name = models.CharField(max_length=100, unique=True,
+        validators=[
+            RegexValidator(
+                regex=r'^[a-zA-Z\s]+$',
+                message="Title must contain only letters and spaces."
+            )
+        ])
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -11,14 +17,28 @@ class Category(models.Model):
         return self.name
 
 class Product(models.Model):
-    title = models.CharField(max_length=200)
+    title = models.CharField(
+        max_length=200,
+        validators=[
+            RegexValidator(
+                regex=r'^[a-zA-Z\s]+$',
+                message="Title must contain only letters and spaces."
+            )
+        ]
+    )
     description = models.TextField()
-    price = models.PositiveIntegerField()
-    rating = models.FloatField(validators=[MinValueValidator(0), MaxValueValidator(5)])
+    price = models.PositiveIntegerField(
+        validators=[MaxValueValidator(10000)],
+        help_text="Price should not exceed 10,000."
+    )
+    rating = models.FloatField(
+        validators=[MinValueValidator(0), MaxValueValidator(5)],
+        help_text="Rating must be between 0 and 5."
+    )
     tags = models.TextField(null=True, blank=True)
     thumbnail = models.URLField()
     category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name="products")
-    cover = models.ImageField(upload_to="products/%Y/%m/%d/" ,null=True ,blank=True)
+    cover = models.ImageField(upload_to="products/%Y/%m/%d/", null=True, blank=True)
 
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
