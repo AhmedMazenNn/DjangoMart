4ce94b10820e24959f4230a2c65508d7f1d0206a
Ahmed Mazen <ahmedmazenm@outlook.com>
2025-03-04 04:37:53 +0200

    updated apps.py forms.py models.py views.py and added signals.py to add new profiles by user and add image and user can update his info

--- Products-lab/products/urls.py
+++ Products-lab/products/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
-    path("all_products/", ProductListView.as_view(), name="all_products"),
+    path("", ProductListView.as_view(), name="all_products"),
     path("product/<int:pk>/", ProductDetailView.as_view(), name="one_product"),
     path("product/add/", ProductCreateView.as_view(), name="add_product"),
     path("product/<int:pk>/edit/", ProductUpdateView.as_view(), name="edit_product"),
