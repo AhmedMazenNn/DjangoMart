81b60cf867501522608a8915be7ba0f8dd7c8b26
Ahmed Mazen <ahmedmazenm@outlook.com>
2025-04-09 00:31:43 +0200

    updated settings

--- Products/settings.py
+++ Products/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-j-wq=sb17q^308ycrvlltvy$%#bay@%3udrrc-zl52+)0ccs-z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
