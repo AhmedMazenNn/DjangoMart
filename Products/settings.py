bd6e8a24168274cb97fbf6b92aaf4b331e31f58a
Ahmed Mazen <ahmedmazenm@outlook.com>
2025-04-09 21:16:38 +0200

    deploy on vercel

--- Products/settings.py
+++ Products/settings.py
@@ -134,7 +134,10 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_URL_DIRS = [BASE_DIR / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
-MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')
+STATICFILES_DIRS = [
+    BASE_DIR / 'static',
+]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
